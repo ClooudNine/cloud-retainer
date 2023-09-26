acecba28313acd2a347fb47d5d10f0a9c17b0761
Uladzislau Syrel <syrel.vladislav@gmail.com>
2023-09-26 22:54:28 +0300

    Fix dynamic server error

--- app/wish-simulator/components/WishSimulatorBanner.tsx
+++ app/wish-simulator/components/WishSimulatorBanner.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 import {createServerComponentClient} from '@supabase/auth-helpers-nextjs'
 import {cookies} from "next/headers";
 const WishSimulatorBanner = () => {
-    const supabase = createServerComponentClient({cookies});
+    const cookieStore = cookies()
+    const supabase = createServerComponentClient({ cookies: () => cookieStore });
     const bannerUrl = supabase.storage.from('wish banners').getPublicUrl('Gentry of Hermitage 5.png').data.publicUrl;
     return (
         <section className={"w-full h-full flex items-center justify-center"}>
