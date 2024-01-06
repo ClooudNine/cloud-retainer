a21d9e6052057b2283900b793b4fd3b55c39dabc
Uladzislau Syrel <syrel.vladislav@gmail.com>
2024-01-07 01:40:13 +0300

    make wish simulator page dynamic

--- app/wish-simulator/page.tsx
+++ app/wish-simulator/page.tsx
@@ -1,99 +1,103 @@
-import Footer from "@/app/wish-simulator/components/footerComponents/Footer";
-import Header from "@/app/wish-simulator/components/headerComponents/Header";
-import Background from "@/app/wish-simulator/components/Background";
-import Banner from "@/app/wish-simulator/components/bannerOverview/Banner";
-import BannerProvider from "@/app/wish-simulator/components/BannerProvider";
+import Footer from '@/app/wish-simulator/components/footerComponents/Footer';
+import Header from '@/app/wish-simulator/components/headerComponents/Header';
+import Background from '@/app/wish-simulator/components/Background';
+import Banner from '@/app/wish-simulator/components/bannerOverview/Banner';
+import BannerProvider from '@/app/wish-simulator/components/BannerProvider';
 import {
-  CharacterBanner,
-  WeaponBanner,
-  characterBanners,
-  weaponBanners,
-  StandardBanner,
-  standardBanners,
-  Character,
-  characters,
-  Weapon,
-  weapons,
-} from "@/lib/db/schema";
-import { db } from "@/lib/db";
-import { isNotNull } from "drizzle-orm";
+    CharacterBanner,
+    WeaponBanner,
+    characterBanners,
+    weaponBanners,
+    StandardBanner,
+    standardBanners,
+    Character,
+    characters,
+    Weapon,
+    weapons,
+    Phases,
+} from '@/lib/db/schema';
+import { db } from '@/lib/db';
+import { eq, isNotNull } from 'drizzle-orm';
 
 export const metadata = {
-  title: "Cloud Retainer | Симулятор молитв",
-  description:
-    "Симулятор молитв из игры Genshin Impact, который позволяет путешественникам совершать молитвы в неограниченном количестве для развлечения и сбора статистики.",
+    title: 'Cloud Retainer | Симулятор молитв',
+    description:
+        'Симулятор молитв из игры Genshin Impact, который позволяет путешественникам совершать молитвы в неограниченном количестве для развлечения и сбора статистики.',
 };
-export default async function WishSimulator() {
-  const allCharactersBanners: CharacterBanner[] = await db
-    .select()
-    .from(characterBanners);
+export default async function WishSimulator({
+    searchParams,
+}: {
+    searchParams: { version: number; phase: Phases };
+}) {
+    const allCharactersBanners: CharacterBanner[] = await db
+        .select()
+        .from(characterBanners);
 
-  const allWeaponBanners: WeaponBanner[] = await db
-    .select()
-    .from(weaponBanners);
+    const allWeaponBanners: WeaponBanner[] = await db
+        .select()
+        .from(weaponBanners)
+        .where(eq(weaponBanners.version, searchParams.version));
 
-  const allStandardBanners: StandardBanner[] = await db
-    .select()
-    .from(standardBanners);
+    const allStandardBanners: StandardBanner[] = await db.select().from(standardBanners);
 
-  const charactersFromWishes: Character[] = await db
-    .select()
-    .from(characters)
-    .where(isNotNull(characters.inStandardWish));
+    const charactersFromWishes: Character[] = await db
+        .select()
+        .from(characters)
+        .where(isNotNull(characters.inStandardWish));
 
-  const weaponsFromWishes: Weapon[] = await db
-    .select()
-    .from(weapons)
-    .where(isNotNull(weapons.inStandardWish));
+    const weaponsFromWishes: Weapon[] = await db
+        .select()
+        .from(weapons)
+        .where(isNotNull(weapons.inStandardWish));
 
-  if (
-    allCharactersBanners === null ||
-    allWeaponBanners === null ||
-    allStandardBanners === null
-  ) {
+    if (
+        allCharactersBanners === null ||
+        allWeaponBanners === null ||
+        allStandardBanners === null
+    ) {
+        return (
+            <div
+                className={
+                    'w-full h-full flex justify-center items-center text-9xl font-genshin'
+                }
+            >
+                Banners fetch error!
+            </div>
+        );
+    }
+
+    if (charactersFromWishes === null || weaponsFromWishes === null) {
+        return (
+            <div
+                className={
+                    'w-full h-full flex justify-center items-center text-9xl font-genshin'
+                }
+            >
+                Characters or weapons fetch error!
+            </div>
+        );
+    }
+
     return (
-      <div
-        className={
-          "w-full h-full flex justify-center items-center text-9xl font-genshin"
-        }
-      >
-        Banners fetch error!
-      </div>
+        <main
+            className={
+                'w-full h-full cursor-genshin grid grid-rows-[1fr_2.5fr_1fr] md:grid-rows-[1fr_5fr_1fr] overflow-hidden'
+            }
+        >
+            <Background isBlurred={false} />
+            <BannerProvider
+                banners={[
+                    ...allCharactersBanners,
+                    ...allWeaponBanners,
+                    ...allStandardBanners,
+                ]}
+                characters={charactersFromWishes}
+                weapons={weaponsFromWishes}
+            >
+                <Header />
+                <Banner />
+                <Footer />
+            </BannerProvider>
+        </main>
     );
-  }
-
-  if (charactersFromWishes === null || weaponsFromWishes === null) {
-    return (
-      <div
-        className={
-          "w-full h-full flex justify-center items-center text-9xl font-genshin"
-        }
-      >
-        Characters or weapons fetch error!
-      </div>
-    );
-  }
-
-  return (
-    <main
-      className={
-        "w-full h-full cursor-genshin grid grid-rows-[1fr_2.5fr_1fr] md:grid-rows-[1fr_5fr_1fr] overflow-hidden"
-      }
-    >
-      <Background isBlurred={false} />
-      <BannerProvider
-        banners={[
-          ...allCharactersBanners,
-          ...allWeaponBanners,
-          ...allStandardBanners,
-        ]}
-        characters={charactersFromWishes}
-        weapons={weaponsFromWishes}
-      >
-        <Header />
-        <Banner />
-        <Footer />
-      </BannerProvider>
-    </main>
-  );
 }
