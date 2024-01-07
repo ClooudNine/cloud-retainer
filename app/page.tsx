1e4065cefe36d6d078400feccc47f9e054e97730
Uladzislau Syrel <syrel.vladislav@gmail.com>
2024-01-07 16:44:10 +0300

    remove wish simulator link from main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Link from "next/link";
 export default function Main() {
-  return (
-    <main>
-      <h1>Cloud Retainer</h1>
-      <Link href={"/wish-simulator"}>Симулятор молитв</Link>
-    </main>
-  );
+    return (
+        <main>
+            <h1>Cloud Retainer</h1>
+        </main>
+    );
 }
