d8ae2aa76c2d310c7bcc8ad273a9eec1d7564805
Uladzislau Syrel <syrel.vladislav@gmail.com>
2024-01-07 16:52:21 +0300

    return link to wish simulator on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Link from 'next/link';
 export default function Main() {
     return (
         <main>
             <h1>Cloud Retainer</h1>
+            <Link href={'/wish-simulator'}>Симулятор молитв</Link>
         </main>
     );
 }
