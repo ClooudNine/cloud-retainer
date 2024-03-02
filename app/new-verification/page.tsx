0b33ef96d0cba5c2d734a7847f015f4f2a741751
Uladzislau Syrel <syrel.vladislav@gmail.com>
2024-03-02 23:19:47 +0300

    fix dynamic error

--- app/new-verification/page.tsx
+++ app/new-verification/page.tsx
@@ -1,6 +1,6 @@
 import NewVerificationForm from '@/components/main-page/new-verification-form';
 import { Suspense } from 'react';
-
+export const dynamic = 'force-dynamic';
 export default function NewVerificationPage() {
     return (
         <Suspense>
